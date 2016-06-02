3a145a4ce2d7ca77ea018a1e692f89299af1db67
Oliver Stewart <oliver@trailmagic.com>
2016-06-02 12:36:33 -0400

    Fix math (this is why reasonable people write tests)

--- public/javascripts/initerate.js
+++ public/javascripts/initerate.js
@@ -13,15 +13,15 @@ define(["moment","d3"], function(moment,d3) {
             var iterationDates = genIterations(startDateMoment, plannedIterations);
             var cumulativePoints = 0;
             var rabuData = iterationPoints.map(function (velocity, i) {
-                var remainingIterations = plannedIterations - i;
+                var remainingIterations = plannedIterations - 1 - i;
                 var stableVelocity = d3.mean(iterationPoints.slice(Math.max(0, i-2),i+1));
                 var idealPoints = remainingIterations * stableVelocity;
                 var futurePoints = [roundPoints(idealPoints/1.8), roundPoints(idealPoints/1.4), roundPoints(idealPoints)];
 
+                cumulativePoints += velocity;
                 var totalPointsSpread = futurePoints.map(function (p) {
                     return p + cumulativePoints;
                 });
-                cumulativePoints += velocity;
                 return {
                     date: startDateMoment.clone().add('weeks', i).valueOf(),
                     spread: totalPointsSpread,
